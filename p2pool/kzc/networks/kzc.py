67bd288326514ab3a0c561357c431455bc681248
ilsawa <ils2000@mail.ru>
2017-11-06 23:40:51 +0600

    first commit

---
+++ p2pool/kzc/networks/kzc.py
@@ -0,0 +1,28 @@
+import os
+import platform
+
+from twisted.internet import defer
+
+from .. import data, helper
+from p2pool.util import pack
+
+
+P2P_PREFIX = 'bd1b44ba'.decode('hex')
+P2P_PORT = 8277
+ADDRESS_VERSION = 46
+SCRIPT_ADDRESS_VERSION = 16
+RPC_PORT = 8276
+RPC_CHECK = defer.inlineCallbacks(lambda kzcd: defer.returnValue(
+            'kzcashaddress' in (yield kzcd.rpc_help()) and
+            not (yield kzcd.rpc_getinfo())['testnet']
+        ))
+BLOCKHASH_FUNC = lambda data: pack.IntType(256).unpack(__import__('dash_hash').getPoWHash(data))
+POW_FUNC = lambda data: pack.IntType(256).unpack(__import__('dash_hash').getPoWHash(data))
+BLOCK_PERIOD = 150
+SYMBOL = 'KZC'
+CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'kzcashcore') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/kzcashcore/') if platform.system() == 'Darwin' else os.path.expanduser('~/.kzcashcore'), 'kzc.conf')
+BLOCK_EXPLORER_URL_PREFIX = 'http://chain.kzcash.kz/block/'
+ADDRESS_EXPLORER_URL_PREFIX = 'http://chain.kzcash.kz/address/'
+TX_EXPLORER_URL_PREFIX = 'http://chain.kzcash.kz/tx/'
+SANE_TARGET_RANGE = (2**256//2**32//1000000 - 1, 2**256//2**32 - 1)
+DUST_THRESHOLD = 0.001e8
---
+++ p2pool/networks/kzc.py
@@ -0,0 +1,19 @@
+from p2pool.kzc import networks
+
+PARENT = networks.nets['kzc']
+SHARE_PERIOD = 7
+CHAIN_LENGTH = 24*60*60//7
+REAL_CHAIN_LENGTH = 24*60*60//7
+TARGET_LOOKBEHIND = 100
+SPREAD = 10
+IDENTIFIER = '1bfe373cce7684cd'.decode('hex')
+PREFIX = '1bfe373ccf0f1b4e'.decode('hex')
+COINBASEEXT = '2f5032506f6f6c2d4b5a432f'.decode('hex')
+P2P_PORT = 8278
+MIN_TARGET = 4
+MAX_TARGET = 2**256//2**20 - 1
+PERSIST = True
+WORKER_PORT = 8279
+BOOTSTRAP_ADDRS = '82.200.205.30'.split(' ')
+ANNOUNCE_CHANNEL = '#p2pool-kzc'
+VERSION_CHECK = lambda v: v >= 10701
