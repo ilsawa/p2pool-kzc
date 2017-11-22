b096a5c3458c8f77c70cd7d0bd3a819f305967e3
ilsawa <ilsawa@users.noreply.github.com>
2017-11-22 12:33:57 +0600

    Increased the period of shares

--- p2pool/networks/kzc.py
+++ p2pool/networks/kzc.py
@@ -1,19 +1,19 @@
 from p2pool.kzc import networks
 
 PARENT = networks.nets['kzc']
-SHARE_PERIOD = 7
-CHAIN_LENGTH = 24*60*60//7
-REAL_CHAIN_LENGTH = 24*60*60//7
+SHARE_PERIOD = 25
+CHAIN_LENGTH = 24*60*60//25
+REAL_CHAIN_LENGTH = 24*60*60//25
 TARGET_LOOKBEHIND = 100
 SPREAD = 10
-IDENTIFIER = '1bfe373cce7684cd'.decode('hex')
-PREFIX = '1bfe373ccf0f1b4e'.decode('hex')
+IDENTIFIER = '1bfe3868e2b7a04f'.decode('hex')
+PREFIX = '1bfe3868e64b2750'.decode('hex')
 COINBASEEXT = '2f5032506f6f6c2d4b5a432f'.decode('hex')
 P2P_PORT = 8278
 MIN_TARGET = 4
 MAX_TARGET = 2**256//2**20 - 1
 PERSIST = True
 WORKER_PORT = 8279
-BOOTSTRAP_ADDRS = 'crypto.office-on-the.net tomsk.mine.nu pool.penza.me mine.janche.ru'.split(' ')
+BOOTSTRAP_ADDRS = 'crypto.mine.nu tomsk.mine.nu p2p-spb.xyz'.split(' ')
 ANNOUNCE_CHANNEL = '#p2pool-kzc'
 VERSION_CHECK = lambda v: v >= 10701
